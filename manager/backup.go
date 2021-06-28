72289ab1db06d1672697fdbb9944365bddaf696b
JenTing Hsiao <jenting.hsiao@suse.com>
2021-06-28 13:19:20 +0800

    Remove no longer used code
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- manager/backup.go
+++ manager/backup.go
@@ -18,22 +18,6 @@ var (
 	ConflictRetryCount = 5
 )
 
-func UpdateVolumeLastBackup(volumeName string, backupTarget *engineapi.BackupTarget,
-	getVolume func(name string) (*longhorn.Volume, error),
-	updateVolume func(v *longhorn.Volume) (*longhorn.Volume, error)) (err error) {
-
-	defer func() {
-		err = errors.Wrapf(err, "failed to UpdateVolumeLastBackup for %v", volumeName)
-	}()
-
-	backupVolumeMetadataURL := backupstore.EncodeMetadataURL("", volumeName, backupTarget.URL)
-	backupVolume, err := backupTarget.InspectBackupVolumeMetadata(backupVolumeMetadataURL)
-	if err != nil {
-		return err
-	}
-	return SyncVolumeLastBackupWithBackupVolume(volumeName, backupVolume, getVolume, updateVolume)
-}
-
 func SyncVolumeLastBackupWithBackupVolume(volumeName string, backupVolume *engineapi.BackupVolume,
 	getVolume func(name string) (*longhorn.Volume, error),
 	updateVolume func(v *longhorn.Volume) (*longhorn.Volume, error)) (err error) {
@@ -94,7 +78,7 @@ func SyncVolumesLastBackupWithBackupVolumes(backupVolumes map[string]*engineapi.
 		if backupVolumes != nil {
 			// Keep updating last backup for restore/DR volumes
 			if v.Status.RestoreRequired || v.Status.IsStandby {
-				bvName, _, _, err := backupstore.DecodeMetadataURL(v.Spec.FromBackup)
+				_, bvName, _, err := backupstore.DecodeMetadataURL(v.Spec.FromBackup)
 				if err != nil {
 					logrus.Errorf("cannot parse field FromBackup of standby volume %v: %v", v.Name, err)
 					return
